0c6486077b3ef34de78f2aa049061438d4db3104
Bernhard Kircher <bernhardkircher@gmail.com>
2015-11-18 21:13:18 +0100

    initial commit of the webapi testproject with basic authentication/attribute and a simple console app to test it

---
+++ WebApiAuthentication.Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiAuthentication.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to start...");
+            Console.ReadLine();
+
+            try
+            {
+                var credentials = new NetworkCredential("test", "test");
+                var handler = new HttpClientHandler { Credentials = credentials };
+
+                using (var client = new HttpClient(handler))
+                {
+                    client.DefaultRequestHeaders.Add("clientid", "world-direct");
+
+                    HttpResponseMessage response = client.GetAsync("http://localhost:59310/api/test").Result;
+                    response.EnsureSuccessStatusCode();
+                    Console.WriteLine("Everything ok");
+                }
+
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApiAuthentication/Controllers/TestController.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebApiAuthentication.Controllers
+{
+    
+
+    public abstract class BasicAuthenticationAttribute : Attribute, IAuthenticationFilter
+    {
+        public string Realm { get; set; }
+
+        public async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            HttpRequestMessage request = context.Request;
+            AuthenticationHeaderValue authorization = request.Headers.Authorization;
+
+            if (authorization == null)
+            {
+                // No authentication was attempted (for this authentication method).
+                // Do not set either Principal (which would indicate success) or ErrorResult (indicating an error).
+                return;
+            }
+
+            if (authorization.Scheme != "Basic")
+            {
+                // No authentication was attempted (for this authentication method).
+                // Do not set either Principal (which would indicate success) or ErrorResult (indicating an error).
+                return;
+            }
+
+            if (string.IsNullOrEmpty(authorization.Parameter))
+            {
+                // Authentication was attempted but failed. Set ErrorResult to indicate an error.
+                context.ErrorResult = new AuthenticationFailureResult("Missing credentials", request);
+                return;
+            }
+
+            Tuple<string, string> userNameAndPasword = ExtractUserNameAndPassword(authorization.Parameter);
+
+            if (userNameAndPasword == null)
+            {
+                // Authentication was attempted but failed. Set ErrorResult to indicate an error.
+                context.ErrorResult = new AuthenticationFailureResult("Invalid credentials", request);
+                return;
+            }
+
+            string userName = userNameAndPasword.Item1;
+            string password = userNameAndPasword.Item2;
+            var clientIdHeader = context.Request.Headers.FirstOrDefault(x => x.Key == "clientid");
+
+            IPrincipal principal = await AuthenticateAsync(userName, password, clientIdHeader, cancellationToken);
+
+            if (principal == null)
+            {
+                // Authentication was attempted but failed. Set ErrorResult to indicate an error.
+                context.ErrorResult = new AuthenticationFailureResult("Invalid username or password", request);
+            }
+            else
+            {
+                // Authentication was attempted and succeeded. Set Principal to the authenticated user.
+                context.Principal = principal;
+            }
+        }
+
+        protected abstract Task<IPrincipal> AuthenticateAsync(string userName, string password, KeyValuePair<string, IEnumerable<string>> clientIdHeader,
+            CancellationToken cancellationToken);
+
+        private static Tuple<string, string> ExtractUserNameAndPassword(string authorizationParameter)
+        {
+            byte[] credentialBytes;
+
+            try
+            {
+                credentialBytes = Convert.FromBase64String(authorizationParameter);
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+
+            // The currently approved HTTP 1.1 specification says characters here are ISO-8859-1.
+            // However, the current draft updated specification for HTTP 1.1 indicates this encoding is infrequently
+            // used in practice and defines behavior only for ASCII.
+            Encoding encoding = Encoding.ASCII;
+            // Make a writable copy of the encoding to enable setting a decoder fallback.
+            encoding = (Encoding)encoding.Clone();
+            // Fail on invalid bytes rather than silently replacing and continuing.
+            encoding.DecoderFallback = DecoderFallback.ExceptionFallback;
+            string decodedCredentials;
+
+            try
+            {
+                decodedCredentials = encoding.GetString(credentialBytes);
+            }
+            catch (DecoderFallbackException)
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(decodedCredentials))
+            {
+                return null;
+            }
+
+            int colonIndex = decodedCredentials.IndexOf(':');
+
+            if (colonIndex == -1)
+            {
+                return null;
+            }
+
+            string userName = decodedCredentials.Substring(0, colonIndex);
+            string password = decodedCredentials.Substring(colonIndex + 1);
+            return new Tuple<string, string>(userName, password);
+        }
+
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            Challenge(context);
+            return Task.FromResult(0);
+        }
+
+        private void Challenge(HttpAuthenticationChallengeContext context)
+        {
+            string parameter;
+
+            if (string.IsNullOrEmpty(Realm))
+            {
+                parameter = null;
+            }
+            else
+            {
+                // A correct implementation should verify that Realm does not contain a quote character unless properly
+                // escaped (precededed by a backslash that is not itself escaped).
+                parameter = "realm=\"" + Realm + "\"";
+            }
+
+            context.ChallengeWith("Basic", parameter);
+        }
+
+        public virtual bool AllowMultiple
+        {
+            get { return false; }
+        }
+    }
+
+    public static class HttpAuthenticationChallengeContextExtensions
+    {
+        public static void ChallengeWith(this HttpAuthenticationChallengeContext context, string scheme)
+        {
+            ChallengeWith(context, new AuthenticationHeaderValue(scheme));
+        }
+
+        public static void ChallengeWith(this HttpAuthenticationChallengeContext context, string scheme, string parameter)
+        {
+            ChallengeWith(context, new AuthenticationHeaderValue(scheme, parameter));
+        }
+
+        public static void ChallengeWith(this HttpAuthenticationChallengeContext context, AuthenticationHeaderValue challenge)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            context.Result = new AddChallengeOnUnauthorizedResult(challenge, context.Result);
+        }
+    }
+
+    public class IdentityBasicAuthenticationAttribute : BasicAuthenticationAttribute
+    {
+        protected override async Task<IPrincipal> AuthenticateAsync(string userName, string password, KeyValuePair<string, IEnumerable<string>> clientIdHeader, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (userName != "test" || password != "test")
+            {
+                // No user with userName/password exists.
+                return null;
+            }
+
+            // TODO add claims as required... use httpheader to get clientId
+            
+
+            // Create a ClaimsIdentity with all the claims for this user.
+            Claim nameClaim = new Claim(ClaimTypes.Name, userName);
+            List<Claim> claims = new List<Claim> { nameClaim };
+
+            // important to set the identity this way, otherwise IsAuthenticated will be false
+            // see: http://leastprivilege.com/2012/09/24/claimsidentity-isauthenticated-and-authenticationtype-in-net-4-5/
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "basic");
+
+            var principal = new ClaimsPrincipal(identity);
+            return principal;
+        }
+
+    }
+
+    public class AddChallengeOnUnauthorizedResult : IHttpActionResult
+    {
+        public AddChallengeOnUnauthorizedResult(AuthenticationHeaderValue challenge, IHttpActionResult innerResult)
+        {
+            Challenge = challenge;
+            InnerResult = innerResult;
+        }
+
+        public AuthenticationHeaderValue Challenge { get; private set; }
+
+        public IHttpActionResult InnerResult { get; private set; }
+
+        public async Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = await InnerResult.ExecuteAsync(cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                // Only add one challenge per authentication scheme.
+                if (!response.Headers.WwwAuthenticate.Any((h) => h.Scheme == Challenge.Scheme))
+                {
+                    response.Headers.WwwAuthenticate.Add(Challenge);
+                }
+            }
+
+            return response;
+        }
+    }
+
+    public class AuthenticationFailureResult : IHttpActionResult
+    {
+        public AuthenticationFailureResult(string reasonPhrase, HttpRequestMessage request)
+        {
+            ReasonPhrase = reasonPhrase;
+            Request = request;
+        }
+
+        public string ReasonPhrase { get; private set; }
+
+        public HttpRequestMessage Request { get; private set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            response.RequestMessage = Request;
+            response.ReasonPhrase = ReasonPhrase;
+            return response;
+        }
+    }
+
+    // http://blog.codeinside.eu/2015/04/17/basic-authentication-in-aspnet-webapi/  and http://aspnet.codeplex.com/sourcecontrol/latest#Samples/WebApi/BasicAuthentication/ReadMe.txt
+    // https://github.com/Code-Inside/Samples/tree/master/2015/WebApiBasicAuth and http://www.rdegges.com/why-i-love-basic-auth/
+    [IdentityBasicAuthentication]
+    [Authorize]
+    public class TestController : ApiController
+    {
+        public string  Get()
+        {
+            return "Everything is OK";
+        }
+
+    }
+}
